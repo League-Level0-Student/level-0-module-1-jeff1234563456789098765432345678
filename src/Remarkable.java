00bb3208f00125cbbca4c3626ccd4810d890033e
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-04-11 18:32:02 -0700

    remarkable,Unbirthday
    :confused:

---
+++ src/Remarkable.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable { public static void main(String[] args) {
+	String shiv = JOptionPane.showInputDialog("what is your name?");
+	if ("Mike".equals(shiv)) {
+		JOptionPane.showMessageDialog(null, ("Your good at music"));
+		if ("Ella".equals(shiv)) {
+			
+					}
+	}JOptionPane.showMessageDialog(null, ("you have 20 frogs!"));
+	
+}{	
+     if ("Daniel".equals(shiv)) {
+	JOptionPane.showMessageDialog(null, ("I'm a good programer!"));
+
+
+}
